54cd1df6bff3258dfe8957227435921b9c2257ee
jn <tupiszakaczki@gmail.com>
2017-08-23 21:01:51 -0400

    zion as POLYGON

--- data-raw/01_borders_download.R
+++ data-raw/01_borders_download.R
@@ -1,5 +1,5 @@
 download_borders <- function(destination_filename){
-        url <- "https://irma.nps.gov/DataStore/DownloadFile/570816"
+        url <- "https://irma.nps.gov/DataStore/DownloadFile/580617"
         download.file(url, destination_filename)
         return(destination_filename)
 }
@@ -9,6 +9,7 @@ preprocess_borders <- function(filename, clean = TRUE){
         st_read(paste0(dir_name, "/nps_boundary.shp")) %>%
                 filter(PARKNAME=="Zion") %>%
                 st_transform(., 26912) %>%
+                st_cast(., "POLYGON") %>%
                 st_write(paste0(dir_name, "/zion.gpkg"))
         if(clean){
                 dir(path=dir_name, pattern="nps_boundary.*", full.names = TRUE) %>%
