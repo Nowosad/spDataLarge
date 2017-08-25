8f4c6fda448338265551510ecf99c262f92371fb
jn <tupiszakaczki@gmail.com>
2017-08-24 20:25:20 -0400

    zion docs

---
+++ R/vectors.R
@@ -0,0 +1,12 @@
+#' Dataset zion
+#'
+#' This is a dataset containing the borders of Zion National Park
+#'
+#' @format A simple feature data frame
+#'
+#' @name zion
+#'
+#' @section zion.gpkg:
+#'
+#' @source \url{https://irma.nps.gov/DataStore/DownloadFile/580617}#'
+NULL
