d47ff240f93a49f9c9b47170b1fbc68004e55362
jn <tupiszakaczki@gmail.com>
2018-06-12 20:44:15 -0400

    travis drat added

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -o errexit -o nounset
+PKG_REPO=$PWD
+cd ..
+
+addToDrat(){
+  mkdir drat; cd drat
+
+  ## Set up Repo parameters
+  git init
+  git config user.name "Jakub Nowosad"
+  git config user.email "nowosad.jakub@gmail.com"
+  git config --global push.default simple
+
+  ## Get drat repo
+  git remote add upstream "https://$GH_TOKEN@github.com/nowosad/drat.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
+
+  Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
+    repodir = '.', \
+    commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
+  git push 2>err.txt
+
+}
+
+addToDrat
+
+## Other options:
+## Only add if the commit is tagged: so something like:
+#if [ $TRAVIS_TAG ] ; then
+#   addToDrat
+#fi
+##but will need to edit .travis.yml since $TRAVIS_BRANCH will now equal $TRAVIS_TAG
+
