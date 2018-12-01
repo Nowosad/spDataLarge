1195a4b65b4e94a3d1d3f5d93c34e8f76b75be0e
Robin Lovelace <rob00x@gmail.com>
2018-12-01 18:59:37 +0000

    Update data and comment usethis code

--- R/bristol_ways.R
+++ R/bristol_ways.R
@@ -19,13 +19,13 @@
 #'         st_set_crs(4326) %>%
 #'         st_sf(data.frame(Name = "Bristol (OSM)"), geometry = .$geometry)
 #' mapview::mapview(bristol_region)
-#' usethis::use_data(bristol_region, overwrite = TRUE)
+#' # usethisis::use_data(bristol_region, overwrite = TRUE)
 #' bristol_ttwa = ttwa_simple %>%
 #'         filter(ttwa11nm == "Bristol") %>%
 #'         select(Name = ttwa11nm)
 #' bristol_ttwa$Name = "Bristol (TTWA)"
 #' mapview::mapview(bristol_ttwa)
-#' usethis::use_data(bristol_ttwa, overwrite = TRUE)
+#' # usethisis::use_data(bristol_ttwa, overwrite = TRUE)
 #' bristol_cents = st_centroid(msoa2011_vsimple)[bristol_ttwa, ]
 #' plot(bristol_cents$geometry)
 #' bristol_zones = msoa2011_vsimple[msoa2011_vsimple$msoa11cd %in% bristol_cents$msoa11cd, ] %>%
@@ -52,8 +52,8 @@
 #'        filter(o %in% bristol_zones$geo_code & d %in% bristol_zones$geo_code, all > 19)
 #' summary(bristol_zones$geo_code %in% bristol_od$d)
 #' summary(bristol_zones$geo_code %in% bristol_od$o)
-#' usethis::use_data(bristol_zones, overwrite = TRUE)
-#' usethis::use_data(bristol_od, overwrite = TRUE)
+#' # usethisis::use_data(bristol_zones, overwrite = TRUE)
+#' # usethisis::use_data(bristol_od, overwrite = TRUE)
 #' od_intra = filter(bristol_od, o == d)
 #' od_inter = filter(bristol_od, o != d)
 #' desire_lines = od2line(od_inter, bristol_zones)
@@ -61,7 +61,7 @@
 #' desire_carshort = dplyr::filter(desire_lines, car_driver > 300 & distance < 5000)
 #' route_carshort = stplanr::line2route(desire_carshort, route_fun = route_osrm)
 #' route_carshort = st_as_sf(route_carshort)
-#' usethis::use_data(route_carshort, overwrite = TRUE)
+#' # usethisis::use_data(route_carshort, overwrite = TRUE)
 #' bb = st_bbox(bristol_ttwa)
 #' ways_road = opq(bbox = bb) %>%
 #'         add_osm_feature(key = "highway",
@@ -80,7 +80,7 @@
 #'         sort() %>%
 #'         head()
 #' bristol_stations = bristol_stations %>% select(name)
-#' usethis::use_data(bristol_stations, overwrite = TRUE)
+#' # usethisis::use_data(bristol_stations, overwrite = TRUE)
 #' ways = st_intersection(res$osm_lines, bristol_ttwa)
 #' ways$highway = as.character(ways$highway)
 #' ways$highway[ways$railway == "rail"] = "rail"
@@ -93,7 +93,7 @@
 #' # convert to linestring
 #' bristol_ways = st_cast(ways, "LINESTRING")
 #' summary(st_geometry(bristol_ways))
-#' usethis::use_data(bristol_ways, overwrite = TRUE)
+#' # usethisis::use_data(bristol_ways, overwrite = TRUE)
 #' }
 "bristol_ways"
 
