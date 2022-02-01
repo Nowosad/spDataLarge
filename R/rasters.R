479641965d96494ef6036cb5e854612773b2be69
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-02-01 14:37:50 +0100

    improves external files documentation

--- R/rasters.R
+++ R/rasters.R
@@ -1,3 +1,29 @@
+#' Digital elevation model (DEM) of the Mongón study area.
+#'
+#' A raster file (EPSG:32717) representing altitude
+#'   (ASTER GDEM, LP DAAC 2012).  For more details, please refer to Muenchow et
+#'   al. (2013).
+#'
+#' @format A geotiff file
+#'
+#' @examples
+#'
+#' system.file("raster/dem.tif", package = "spDataLarge")
+#'
+#' @name dem.tif
+#'
+#' @references Muenchow, J., Bräuning, A., Rodríguez, E.F. & von Wehrden, H.
+#' (2013): Predictive mapping of species richness and plant species'
+#' distributions of a Peruvian fog oasis along an altitudinal gradient.
+#' Biotropica 45, 5, 557-566, doi: 10.1111/btp.12049.
+#'
+#' LP DAAC (2012): Land Processes Distributed Active Archive Center, located at
+#' the U.S. Geological Survey (USGS) Earth Resources Observation and Science
+#' (EROS) Center. Available at: https://lpdaac.usgs.gov/ (last accessed 25
+#' January 2012).
+NULL
+
+
 #' Dataset landsat
 #'
 #' This is a dataset containing the four bands (2, 3, 4, 5) of the Landsat 8 image for
@@ -5,36 +31,38 @@
 #'
 #' @format A multilayer geotiff file
 #'
-#' @name landsat
+#' @examples
+#'
+#' system.file("raster/landsat.tif", package = "spDataLarge")
+#'
+#' @name landsat.tif
 #'
 #'
 #' @source \url{http://landsat-pds.s3.amazonaws.com/L8/038/034/LC80380342015230LGN00/}
 NULL
 
-#' Dataset srtm
+#' Normalized difference vegetation index for the Mongón study area.
 #'
-#' This is a dataset containing the elevation raster data from SRTM of
-#' the Zion National Park area
+#' NDVI raster file (EPSG:32717) computed from a Landsat
+#'   scene (path 9, row 67, acquisition date: 09/22/2000; USGS 2013). For more
+#'   details, please refer to Muenchow et al. (2013).
+#'
 #'
 #' @format A geotiff file
 #'
-#' @name srtm
+#' @examples
 #'
+#' system.file("raster/ndvi.tif", package = "spDataLarge")
 #'
-#' @source \url{http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/srtm_14_05.zip}
-NULL
-
-#' Dataset nlcd2011
+#' @name ndvi.tif
 #'
-#' This is a dataset containing the National Land Cover Database 2011 product for
-#' the Zion National Park area
+#' @references Muenchow, J., Bräuning, A., Rodríguez, E.F. & von Wehrden, H.
+#' (2013): Predictive mapping of species richness and plant species'
+#' distributions of a Peruvian fog oasis along an altitudinal gradient.
+#' Biotropica 45, 5, 557-566, doi: 10.1111/btp.12049.
 #'
-#' @format A geotiff file
-#'
-#' @name nlcd2011
-#'
-#'
-#' @source \url{https://www.mrlc.gov/nlcd2011.php}
+#' USGS (2013): U.S. Geological Survey. Earth Explorer. Available at:
+#' http://earthexplorer.usgs.gov/ (last accessed 1 March 2013).
 NULL
 
 #' Dataset nlcd
@@ -44,8 +72,66 @@ NULL
 #'
 #' @format A geotiff file
 #'
-#' @name nlcd
+#' @examples
 #'
+#' system.file("raster/nlcd.tif", package = "spDataLarge")
+#'
+#' @name nlcd.tif
 #'
 #' @source \url{https://www.mrlc.gov/nlcd2011.php}
+NULL
+
+
+#' Dataset nlcd2011
+#'
+#' This is a dataset containing the National Land Cover Database 2011 product for
+#' the Zion National Park area
+#'
+#' @format A geotiff file
+#'
+#' @examples
+#'
+#' system.file("raster/nlcd2011.tif", package = "spDataLarge")
+#'
+#' @name nlcd2011.tif
+#'
+#' @source \url{https://www.mrlc.gov/nlcd2011.php}
+NULL
+
+
+
+#' New Zeleand elevation raster data
+#'
+#' Elevation raster data of the New Zealand area from the Mapzen Terrain Service.
+#' For teaching purposes only
+#'
+#' @format A geotiff file
+#'
+#' @examples
+#'
+#' system.file("raster/nz_elev.tif", package = "spDataLarge")
+#'
+#' @name nz_elev.tif
+#'
+#' @seealso
+#' See the elevatr package: https://cran.r-project.org/web/packages/elevatr
+#'
+#' @source \url{https://aws.amazon.com/public-datasets/terrain/}
+NULL
+
+
+#' Dataset srtm
+#'
+#' This is a dataset containing the elevation raster data from SRTM of
+#' the Zion National Park area
+#'
+#' @format A geotiff file
+#'
+#' @examples
+#'
+#' system.file("raster/srtm.tif", package = "spDataLarge")
+#'
+#' @name srtm.tif
+#'
+#' @source \url{http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/srtm_14_05.zip}
 NULL
--- R/vectors.R
+++ R/vectors.R
@@ -1,10 +1,29 @@
+#' Point vector data
+#'
+#' Dataset containing 30 randomly located points in the Zion National Park
+#'
+#' @format A geopackage file
+#'
+#' @examples
+#'
+#' system.file("vector/zion_points.gpkg", package = "spDataLarge")
+#'
+#' @name zion_points.gpkg
+#'
+#' @source \url{https://irma.nps.gov/DataStore/DownloadFile/580617}
+NULL
+
 #' Dataset zion
 #'
 #' This is a dataset containing the borders of Zion National Park
 #'
 #' @format A geopackage file
 #'
-#' @name zion
+#' @examples
+#'
+#' system.file("vector/zion.gpkg", package = "spDataLarge")
+#'
+#' @name zion.gpkg
 #'
 #' @source \url{https://irma.nps.gov/DataStore/DownloadFile/580617}
 NULL
