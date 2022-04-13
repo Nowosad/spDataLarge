0fce3a987dd37e915ab46149a8c148c11d2171d5
jannes-m <malnamalja@gmx.de>
2022-04-13 11:09:24 +0200

    put all data needed for the ecology chapter into R/mongon.R

--- R/rasters.R
+++ R/rasters.R
@@ -1,58 +1,3 @@
-#' Data used in the "Ecology" chapter in Geocomputation with R.
-#' See \url{https://geocompr.robinlovelace.net/eco.html} for details.
-#'
-#' @format A geotiff file (CRS: UTM zone 17S; EPSG:32717) with 117 rows and 117 columns:
-#' \enumerate{
-#'     \item{\code{dem}} {Digital elevation model (ASTER GDEM, LP DAAC 2012), see also [spDataLarge::dem].}
-#'     \item{\code{ndvi}} {Normalized Differenced Vegetation index.}
-#'     \item{\code{carea}} {Catchment area.}
-#'     \item{\code{cslope}} {Catchment slope}
-#'     }
-#' @source
-#' LP DAAC (2012): Land Processes Distributed Active Archive Center, located
-#' at the U.S. Geological Survey (USGS) Earth Resources Observation
-#' and Science (EROS) Center. Available at: https://lpdaac.usgs.gov/
-#' (last accessed 25 January 2012).
-#'
-#' Muenchow, J., Brauning, A., Rodriguez, E.F. & von Wehrden, H. (2013):
-#' Predictive mapping of species richness and plant species' distributions of a
-#' Peruvian fog oasis along an altitudinal gradient. Biotropica 45, 5, 557-566,
-#' doi: 10.1111/btp.12049.
-#'
-#' Muenchow, J., Schratz, P., and A. Brenning. 2017. RQGIS: Integrating R with
-#' QGIS for Statistical Geocomputing. The R Journal 9, 2, 409-428.
-#' https://rjournal.github.io/archive/2017/RJ-2017-067/RJ-2017-067.pdf.
-#'
-#' @examples
-#' system.file("raster/ep.tif", package = "spDataLarge")
-NULL
-
-#' Digital elevation model (DEM) of the Mongón study area.
-#'
-#' A raster file (EPSG:32717) representing altitude
-#'   (ASTER GDEM, LP DAAC 2012).  For more details, please refer to Muenchow et
-#'   al. (2013).
-#'
-#' @format A geotiff file
-#'
-#' @examples
-#'
-#' system.file("raster/dem.tif", package = "spDataLarge")
-#'
-#' @name dem.tif
-#'
-#' @references Muenchow, J., Bräuning, A., Rodríguez, E.F. & von Wehrden, H.
-#' (2013): Predictive mapping of species richness and plant species'
-#' distributions of a Peruvian fog oasis along an altitudinal gradient.
-#' Biotropica 45, 5, 557-566, doi: 10.1111/btp.12049.
-#'
-#' LP DAAC (2012): Land Processes Distributed Active Archive Center, located at
-#' the U.S. Geological Survey (USGS) Earth Resources Observation and Science
-#' (EROS) Center. Available at: https://lpdaac.usgs.gov/ (last accessed 25
-#' January 2012).
-NULL
-
-
 #' Dataset landsat
 #'
 #' This is a dataset containing the four bands (2, 3, 4, 5) of the Landsat 8 image for
@@ -68,30 +13,6 @@ NULL
 #'
 #'
 #' @source \url{http://landsat-pds.s3.amazonaws.com/L8/038/034/LC80380342015230LGN00/}
-NULL
-
-#' Normalized difference vegetation index for the Mongón study area.
-#'
-#' NDVI raster file (EPSG:32717) computed from a Landsat
-#'   scene (path 9, row 67, acquisition date: 09/22/2000; USGS 2013). For more
-#'   details, please refer to Muenchow et al. (2013).
-#'
-#'
-#' @format A geotiff file
-#'
-#' @examples
-#'
-#' system.file("raster/ndvi.tif", package = "spDataLarge")
-#'
-#' @name ndvi.tif
-#'
-#' @references Muenchow, J., Bräuning, A., Rodríguez, E.F. & von Wehrden, H.
-#' (2013): Predictive mapping of species richness and plant species'
-#' distributions of a Peruvian fog oasis along an altitudinal gradient.
-#' Biotropica 45, 5, 557-566, doi: 10.1111/btp.12049.
-#'
-#' USGS (2013): U.S. Geological Survey. Earth Explorer. Available at:
-#' http://earthexplorer.usgs.gov/ (last accessed 1 March 2013).
 NULL
 
 #' Dataset nlcd
